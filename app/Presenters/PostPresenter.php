bb8ff9a2c5114c7fae148a56ea8408067978b0e2
Vratislav Beran <beran.vratislav23@gmail.com>
2021-11-27 08:34:44 +0100

    Polovina u Stránka s příspěvkem
    Naposledy je vytvořený show.latte a nakopírovaný obsah

---
+++ app/Presenters/PostPresenter.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+final class PostPresenter extends Nette\Application\UI\Presenter
+{
+	private Nette\Database\Explorer $database;
+
+	public function __construct(Nette\Database\Explorer $database)
+	{
+		$this->database = $database;
+	}
+
+	public function renderShow(int $postId): void
+	{
+		$this->template->post = $this->database
+			->table('posts')
+			->get($postId);
+	}
+}
