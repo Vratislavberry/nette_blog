58f7912594c337857bd36268c7ff1691eb3700a2
Vratislav Beran <beran.vratislav23@gmail.com>
2021-11-27 11:14:02 +0100

    Dokončení sekce Komentáře
    opět jsem přidal pár komentářů do kódu pro lepší čitelnost

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -14,7 +14,7 @@ final class PostPresenter extends Nette\Application\UI\Presenter
 	}
 
 	public function renderShow(int $postId): void
-{
+	{
 	$post = $this->database
 		->table('posts')
 		->get($postId);
@@ -22,6 +22,54 @@ final class PostPresenter extends Nette\Application\UI\Presenter
 		$this->error('Stránka nebyla nalezena');
 	}
 
+
+	// Pridani komentaru
 	$this->template->post = $post;
-}
+	$this->template->comments = $post->related('comments')->order('created_at');
+
+	}
+
+
+	protected function createComponentCommentForm(): Form
+	{
+		$form = new Form; // means Nette\Application\UI\Form
+
+		$form->addText('name', 'Jméno:')
+		// 'name' - jmeno, nejspise v post promenne.
+		// 'Jméno' - label před textovým polem 
+			->setRequired();
+		// >setRequired(); - zajistí, aby dané pole bylo před odeslání vyplněno.
+
+		$form->addEmail('email', 'E-mail:');
+
+		$form->addTextArea('content', 'Komentář:')
+			->setRequired();
+
+		$form->addSubmit('send', 'Publikovat komentář');
+
+		$form->onSuccess[] = [$this, 'commentFormSucceeded'];
+		// Když bude formulář v pořádku vyplněn a odeslán, tak se spustí metoda 
+		// commentFormSucceeded z aktuální třídy (kvůli $this). 
+
+		return $form;
+	}
+
+
+	public function commentFormSucceeded(\stdClass $values): void
+	{
+		$postId = $this->getParameter('postId');
+
+		$this->database->table('comments')->insert([
+			'post_id' => $postId,
+			'name' => $values->name,
+			'email' => $values->email,
+			'content' => $values->content,
+		]);
+
+		$this->flashMessage('Děkuji za komentář', 'success');
+		$this->redirect('this');
+	}
+
+
+
 }
